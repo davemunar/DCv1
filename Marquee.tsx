05a3a32c59c1d1887de105ade7ea8661f45b4146
davemunar <davemunar24@gmail.com>
2025-10-13 11:18:58 -0500

    Production change Marquee1 title

--- Marquee.tsx
+++ Marquee.tsx
@@ -10,7 +10,7 @@ const Marquee: React.FC<MarqueeProps> = ({ className = '' }) => {
     return (
         <div className={`marquee-section ${className}`}>
             {/* Mensaje temporal de Marquee */}
-            ¡🎄 Adelántate a la Navidad! 35% OFF💰 en tus pedidos🎁.
+            ¡🎄Adelántate a diciembre y obtén $ 35% OFF🎁!
         </div>
     );
 };
